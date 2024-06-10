e8ded689b0498bdd52c1ffc6c5715eeaf49f2eea
Jason <j989li@uwaterloo.ca>
2024-06-10 06:21:21 -0400

    Populated data with mock players

--- src/App.js
+++ src/App.js
@@ -10,13 +10,43 @@ const App = () => {
 
   // Mock Data
   const [pairs, setPairs] = useState([
-    { id: 1, players: [createPlayer('p1', 'Player 1', 1), createPlayer('p2', 'Player 2', 2)] },
-    { id: 2, players: [createPlayer('p3', 'Player 3', 3), createPlayer('p4', 'Player 4', 1)] }
+    { id: 1, players: [createPlayer('1', 'Pain boy', 3), createPlayer('2', 'The Origin', 3)] },
+    { id: 2, players: [createPlayer('3', 'Pata-Mon', 3), createPlayer('4', '- Q', 1)] },
+    { id: 3, players: [createPlayer('5', 'Snte4es', 2), createPlayer('6', '[Satori]', 3)] },
+    { id: 4, players: [createPlayer('7', 'ShandenLunaire', 2), createPlayer('8', 'BurgerKing', 1)] },
+    { id: 5, players: [createPlayer('9', 'My Angel Bronya', 3), createPlayer('10', 'suzuka1', 2)] },
+    { id: 6, players: [createPlayer('11', 'Sy_Abyss', 2), createPlayer('12', 'LomOmon', 3)] },
+    { id: 7, players: [createPlayer('13', 'Dichlorocarbene', 2), createPlayer('14', 'Folinic', 3)] },
+    { id: 8, players: [createPlayer('15', 'Tokai-Teio', 2), createPlayer('16', '[-buki-]', 1)] },
+    { id: 9, players: [createPlayer('17', 'Antonilization', 3), createPlayer('18', 'chanliuko', 3)] },
+    { id: 10, players: [createPlayer('19', 'ElectroTD', 3), createPlayer('20', 'Mars New', 2)] },
+    { id: 11, players: [createPlayer('21', 'maduang', 1), createPlayer('22', 'Cake Sauce', 2)] },
+    { id: 12, players: [createPlayer('23', 'K45en', 1), createPlayer('24', 'KiraKiraAyu', 1)] },
+    { id: 13, players: [createPlayer('25', 'pepmoutain', 2), createPlayer('26', '-Mqre-', 1)] },
+    { id: 14, players: [createPlayer('27', 'Hinanawi Momoko', 3), createPlayer('28', 'ZxKirara', 3)] },
   ]);
 
   const [freeAgents, setFreeAgents] = useState([
-    createPlayer('p5', 'Player 5', 2),
-    createPlayer('p6', 'Player 6', 3),
+    createPlayer('29', 'AstonFrenchWine', 1),
+    createPlayer('30', 'miltonfs', 1),
+    createPlayer('31', 'Truth you left', 1),
+    createPlayer('32', 'TwinIslandMilk', 1),
+    createPlayer('33', 'WuYilcft', 1),
+    createPlayer('34', 'babyvzzz', 1),
+    createPlayer('35', 'BG eggman', 1),
+    createPlayer('36', 'QiCaiLieMaxMox', 1),
+    createPlayer('37', 'ShandenYamada', 1),
+    createPlayer('38', 'JD14121', 2),
+    createPlayer('39', 'EveLiya', 2),
+    createPlayer('40', 'Trecont', 2),
+    createPlayer('41', 'TTTtraitor', 2),
+    createPlayer('42', 'ATRI514', 2),
+    createPlayer('43', 'NaimuTongzi', 2),
+    createPlayer('44', '[SomaRia]', 2),
+    createPlayer('45', 'Senbe1', 3),
+    createPlayer('46', 'astralrain', 3),
+    createPlayer('47', 'Fyu_Neru', 3),
+    createPlayer('48', 'Zh_Jk', 3),
   ]);
 
   // const [current, setCurrent] = useState(null);
